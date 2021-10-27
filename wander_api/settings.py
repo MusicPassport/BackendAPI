d3b2d11b0b4661e012db6455dde98e4518b10ba0
sahirabibi <sahirabibi96@gmail.com>
2021-10-26 21:35:45 -0400

    Add button functionality in EventDetail

--- wander_api/settings.py
+++ wander_api/settings.py
@@ -21,10 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['SECRET_KEY']
+# SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = 'ashrkwehriohwkjrehwiu3yersdnbf'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True if os.environ['MODE'] == 'dev' else False
+# DEBUG = True if os.environ['MODE'] == 'dev' else False
+DEBUG='dev'
 
 ALLOWED_HOSTS = ['*']
 
