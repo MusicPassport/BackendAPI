68affcb341480933475a1cd155c2169e939d89b1
shelbyreese47 <shelbyreese47@gmail.com>
2021-10-27 18:00:45 -0600

    change paths in the wander urls to accept alphanumeric

--- wander/urls.py
+++ wander/urls.py
@@ -3,15 +3,15 @@ from . import views
 
 urlpatterns = [
     path('events/', views.EventList.as_view(), name='event_list'),
-    path('events/<int:pk>', views.EventDetail.as_view(), name='event_detail'),
+    path('events/<pk>', views.EventDetail.as_view(), name='event_detail'),
 
     path('venues/', views.VenueList.as_view(), name='venue_list'),
-    path('venues/<int:pk>', views.VenueDetail.as_view(), name='venue_detail'),
+    path('venues/<pk>', views.VenueDetail.as_view(), name='venue_detail'),
 
     path('reviews/', views.ReviewList.as_view(), name='review_list'),
-    path('reviews/<int:pk>', views.ReviewDetail.as_view(), name='review_detail'),
+    path('reviews/<pk>', views.ReviewDetail.as_view(), name='review_detail'),
 
     path('memories/', views.MemoryList.as_view(), name='memory_list'),
-    path('memories/<int:pk>', views.MemoryDetail.as_view(), name='memory_detail'),
+    path('memories/<pk>', views.MemoryDetail.as_view(), name='memory_detail'),
     # path('user/<int:pk>/event/<int:pk>')
 ]
